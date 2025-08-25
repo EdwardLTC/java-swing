001aff1c34dff7c9b0dc951d972d37b4e515c709
vapnguyen24 <vinhnt.work@gmail.com>
2025-08-25 16:12:45 +0700

    feat: handle tab pane keyboard event

--- src/main/java/org/example/presentation/screens/Dashboard.java
+++ src/main/java/org/example/presentation/screens/Dashboard.java
@@ -3,14 +3,14 @@ package org.example.presentation.screens;
 import org.example.presentation.annotations.Screen;
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 @Screen
 public class Dashboard extends JPanel {
     private JPanel panel1;
 
-    private JTabbedPane tabbedPane1;
+    private JTabbedPane tabbedPane;
     private JPanel productPanel;
     private JPanel categoryPanel;
     private JPanel orderPanel;
@@ -23,6 +23,7 @@ public class Dashboard extends JPanel {
         add(panel1, BorderLayout.CENTER);
 
         initPanels();
+        handleKeyboardEvent();
 
     }
 
@@ -32,5 +33,34 @@ public class Dashboard extends JPanel {
         orderPanel.add(new Orders(), BorderLayout.CENTER);
         supplierPanel.add(new Suppliers(), BorderLayout.CENTER);
         accountPanel.add(new Account(), BorderLayout.CENTER);
+    }
+
+    private void handleKeyboardEvent() {
+        System.out.println(tabbedPane.getComponentCount());
+
+        InputMap inputMap = tabbedPane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+        ActionMap actionMap = tabbedPane.getActionMap();
+
+        inputMap.put(KeyStroke.getKeyStroke("LEFT"), "moveLeft");
+        actionMap.put("moveLeft", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int index = tabbedPane.getSelectedIndex();
+                if (index > 0) {
+                    tabbedPane.setSelectedIndex(index - 1);
+                }
+            }
+        });
+
+        inputMap.put(KeyStroke.getKeyStroke("RIGHT"), "moveRight");
+        actionMap.put("moveRight", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int index = tabbedPane.getSelectedIndex();
+                if (index < tabbedPane.getTabCount() - 1) {
+                    tabbedPane.setSelectedIndex(index + 1);
+                }
+            }
+        });
     }
 }
